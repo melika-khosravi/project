7ec0204f2cb73df27c994285f69eb804a0712c5a
melika khosravi <m.khsrv1998@gmail.com>
2024-11-10 15:06:18 -0500

    forth commit-adding port

--- app.py
+++ app.py
@@ -4,11 +4,9 @@ from config import DevelopmentConfig
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
 
-
 @app.route('/')
 def home():
     return "Hello, World!"
 
 if __name__ == '__main__':
-    app.run()
-
+    app.run(host=app.config['HOST'], port=app.config['PORT'])
--- config.py
+++ config.py
@@ -5,6 +5,8 @@ class Config:
     TESTING = False
     SECRET_KEY = os.environ.get("SECRET_KEY", "my_default_secret_key")
     DATABASE_URI = os.environ.get("DATABASE_URI", "sqlite:///my_database.db")
+    PORT = int(os.environ.get("PORT", 5000))
+    HOST = os.environ.get("HOST", "0.0.0.0")
 
 class DevelopmentConfig(Config):
     DEBUG = True
